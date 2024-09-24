a07a4a8ec1ec4b7d5756ef7619e711da9b185631
GeovaneMartinszz <geovanemartins2305@gmail.com>
2024-09-24 09:00:10 -0300

    .

---
+++ ao2_input.py
@@ -0,0 +1,13 @@
+nome = "Asdrubal"
+nome = input("Digite seu nome: ")
+
+print(nome)
+
+n1 = int(input("Digite um numero inteiro: "))
+n2 = input("Digite outro numero inteiro: ")
+n2 = int(n2)
+print(n1 + n2)
+
+Sobrenome = input("Digite seu sobrenome: ")
+idade = input("Digite sua idade: ")
+print(f"Seu nome é {nome}{Sobrenome} e sua idade é {idade}")
