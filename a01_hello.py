e6cafc15bf04bae8a8e3526344471bea60c193ae
Geovane Martins Silva <00001207418742sp@aluno.educacao.sp.gov.br>
2024-09-24 08:27:19 -0300

    git inicial

---
+++ a01_hello.py
@@ -0,0 +1,20 @@
+print("olá, mundo!")
+a = 1
+b = 2
+
+print(a + b)
+
+texto = "esta e uma frase."
+
+print(texto + "9")
+
+a = 15.9 + 3
+
+print(a)
+aumento = True
+print(aumento)
+
+media = 7.3
+novamedia = 7,9
+print(media)
+print(novamedia)
