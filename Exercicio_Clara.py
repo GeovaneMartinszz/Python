b855d1aa2bdfcf5128bb29c0e549c7e99b7ee085
GeovaneMartinszz <geovanemartins2305@gmail.com>
2024-11-05 10:19:14 -0300

    .

---
+++ Exercicio_Clara.py
@@ -0,0 +1,13 @@
+def somar_elementos(Lista):
+    return sum(Lista)
+
+#dados ficticios
+lista1=[1,2,3,4,5] # a soma deve ser 15
+lista2=[10,-2,3.5,7.5] # a soma deve ser 19.0
+lista3=[]            # a soma deve ser 0
+lista4=[-1,-2,-3,-4] # a soma deve ser -10
+
+print("soma da lista1: ", somar_elementos(lista1))
+print("soma da lista2: ", somar_elementos(lista2))
+print("soma da lista3: ", somar_elementos(lista3))
+print("soma da lista4: ", somar_elementos(lista4))
